b1232b6fa343c5b07625bed712d8e91a393923ca
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-04-09 07:14:29 -0600

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// users routers
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show'])->where('id', '[0-9]+');
+
+
+// role ruote
+Route::get('/role', [RoleController::class, 'index']);
+Route::post('/role/', [RoleController::class, 'store']);
+Route::get('/role/{id}', [RoleController::class, 'show'])->where('id', '[0-9]+');
+Route::put('/role/{id}', [RoleController::class, 'update'])->where('id', '[0-9]+');
+
+
+// category route
+Route::get('/category', [CategoryController::class, 'index']);
+Route::get('/category/{id}', [CategoryController::class, 'show'])->where('id', '[0-9]+');
+
+// product route
+Route::get('/product', [ProductController::class, 'index']);
+Route::get('/product/{id}', [ProductController::class, 'show'])->where('id', '[0-9]+');
