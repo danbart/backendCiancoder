6092f223b0e6bc2fd5d7c61dfd18cb6221e00529
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-04-09 16:52:26 -0600

    updated user model and created authController

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -60,5 +62,25 @@ class User extends Authenticatable
     public function buyer()
     {
         return $this->hasMany(Sale::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
