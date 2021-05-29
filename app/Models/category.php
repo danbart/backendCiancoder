240ca3fc2242869a3d7c364428b3e4f9a878b6ee
danbart <danilo_solorzano06@hotmail.com>
2021-05-28 20:47:10 -0600

    update model

--- app/Models/category.php
+++ app/Models/category.php
@@ -9,7 +9,13 @@ class Category extends Model
 {
     use HasFactory;
 
-    protected $table = 'category';
+
+    protected $fillable = [
+        'id',
+        'category',
+        'created_at',
+        'updated_at',
+    ];
 
     public function product()
     {
